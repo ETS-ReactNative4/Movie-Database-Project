9ae96975dae45bfb6934720596a332d98ac46a20
Ryan <rjpadil1@uci.edu>
2019-02-05 21:44:01 -0800

    IT WORKS.

--- cs122b/src/Cart/Cart.js
+++ cs122b/src/Cart/Cart.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Redirect} from "react-router";
 import MyLoader from "../MyLoader/MyLoader";
-import {Button, Container, Form, Grid, Header, Icon, Input, Item, Modal, Rail, Segment} from "semantic-ui-react";
+import {Button, Container, Form, Header, Icon, Input, Modal, Segment} from "semantic-ui-react";
 
 function isEmpty(obj) {
     for (var prop in obj) {
@@ -20,6 +20,37 @@ function EmptyCart() {
         </Container>
     );
 }
+function OrderNums(props){
+    const list = Object.keys(props.list).map((key) =>
+        <Segment key={key}>
+            {
+                props.list[key]
+            }
+        </Segment>
+    );
+    return(
+        <Segment.Group>
+            {
+                list
+            }
+        </Segment.Group>
+    );
+}
+function Orders(props) {
+    const items = Object.keys(props.res).map((key) =>
+        <Segment key={key}>
+            <Header as={'h1'}>
+                {props.movies[key].title}
+            </Header>
+            <OrderNums list={props.res[key]}/>
+        </Segment>
+    );
+    return (
+        <Container>
+            {items}
+        </Container>
+    );
+}
 
 class Cart extends Component {
     state = {
@@ -34,9 +65,21 @@ class Cart extends Component {
             address: '',
             ccId: '',
             expiration: ''
-        }
+        },
+        error: {
+            firstName: false,
+            lastName: false,
+            email: false,
+            address: false,
+            ccId: false,
+            expiration: false,
+        },
+        transaction: false,
+        disabled: true,
+        res: {}
     };
-    constructor(){
+
+    constructor() {
         super();
         this.handleInputChange = this.handleInputChange.bind(this);
     }
@@ -54,8 +97,21 @@ class Cart extends Component {
             let cust = JSON.parse(sessionStorage.getItem("customer"));
             if (!isEmpty(cust)) {
                 this.setState({customer: cust}, function () {
-                    console.log(this.state);
+                    let form = {...this.state.formData};
+                    form["firstName"] = this.state.customer.firstName;
+                    form["lastName"] = this.state.customer.lastName;
+                    form["email"] = this.state.customer.email;
+                    form["address"] = this.state.customer.address;
+                    this.setState({formData: form});
                 });
+            }
+            else{
+                let form = {...this.state.formData};
+                form["firstName"] = this.state.customer.firstName;
+                form["lastName"] = this.state.customer.lastName;
+                form["email"] = this.state.customer.email;
+                form["address"] = this.state.customer.address;
+                this.setState({formData: form});
             }
         });
         fetch('http://' + window.location.hostname + ':8080/cs122b/login', {
@@ -100,31 +156,55 @@ class Cart extends Component {
     componentWillUnmount() {
         this.setState({mounted: false});
     }
+
     handleInputChange(e) {
         let formData = Object.assign({}, this.state.formData);
         formData[e.target.name] = e.target.value;
-        this.setState({formData})
+        let n = e.target.name;
+        let v = e.target.value;
+        this.setState({formData}, function () {
+            let e = {...this.state.error};
+            e[n] = this.state.customer[n] !== v;
+            this.setState({error: e}, function(){
+                let form = this.state.formData;
+                let cust = this.state.customer;
+                let correct = Object.keys(form).every(function(key) {
+                    return form[key] === cust[key];
+                });
+                correct = !correct;
+                this.setState({disabled: correct})
+            });
+        });
     }
 
     handleCheckout = event => {
-        console.log(this.state.formData);
-        // let crt = {...this.state.carty};
-        // crt['customerId'] = this.state.customer["id"];
-        // console.log(crt);
-        // fetch('http://' + window.location.hostname + ':8080/cs122b/sale', {
-        //     method: 'POST',
-        //     headers: {
-        //         "Content-Type": "application/json"
-        //     },
-        //     credentials: 'include',
-        //     body: JSON.stringify(crt)
-        // }).then((res) => res.json())
-        //     .then(
-        //         data => {
-        //             this.setState({records: data});
-        //             console.log(data);
-        //         }
-        //     ).catch((error) => console.log(error))
+        let form = this.state.formData;
+        let cust = this.state.customer;
+        let correct = Object.keys(form).every(function(key) {
+            return form[key] === cust[key];
+        });
+        if (correct) {
+            let crt = {...this.state.carty};
+            crt['customerId'] = this.state.customer["id"];
+            fetch('http://' + window.location.hostname + ':8080/cs122b/sale', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                credentials: 'include',
+                body: JSON.stringify(crt)
+            }).then((res) => res.json())
+                .then(
+                    data => {
+                        this.setState({res: data, transaction: true});
+                        console.log(data);
+                    }
+                ).catch((error) => console.log(error))
+        }
+        else{
+            console.log(form);
+            console.log(cust);
+        }
     };
 
     render() {
@@ -161,47 +241,74 @@ class Cart extends Component {
                         <Container>
                             {items}
                             <Modal closeIcon
-                                trigger={
-                                    <Button color={'green'} fluid content={'Checkout'}/>
-                                }
-                                >
+                                   trigger={
+                                       <Button color={'green'} fluid content={'Checkout'}/>
+                                   }
+                            >
                                 <Header icon={'shopping cart'} content={'Checkout'}/>
                                 <Modal.Content>
                                     <Form>
                                         <Form.Group>
                                             <Form.Input icon={'male'}
                                                         name={'firstName'}
+                                                        defaultValue={this.state.formData.firstName}
                                                         onChange={this.handleInputChange}
+                                                        error={this.state.error['firstName']}
                                                         placeholder={'First Name'}/>
                                             <Form.Input icon={'male'}
                                                         name={'lastName'}
+                                                        defaultValue={this.state.formData.lastName}
                                                         onChange={this.handleInputChange}
+                                                        error={this.state.error['lastName']}
                                                         placeholder={'Last Name'}/>
                                             <Form.Input icon={'mail'}
                                                         name={'email'}
+                                                        defaultValue={this.state.formData.email}
+                                                        error={this.state.error['email']}
                                                         onChange={this.handleInputChange}
                                                         placeholder={'E-mail'}/>
                                         </Form.Group>
                                         <Form.Group>
                                             <Form.Input icon={'address book'}
                                                         name={'address'}
+                                                        defaultValue={this.state.formData.address}
                                                         onChange={this.handleInputChange}
+                                                        error={this.state.error['address']}
                                                         placeholder={'Address'}/>
                                             <Form.Input icon={'credit card'}
                                                         name={'ccId'}
                                                         onChange={this.handleInputChange}
+                                                        error={this.state.error['ccId']}
                                                         placeholder={'Credit Card'}
                                             />
                                             <Form.Input icon={'calendar'}
                                                         name={'expiration'}
                                                         onChange={this.handleInputChange}
+                                                        error={this.state.error['expiration']}
                                                         placeholder={'Expiration Date'}
                                             />
                                         </Form.Group>
                                     </Form>
                                 </Modal.Content>
                                 <Modal.Actions>
-                                    <Button color={'green'} content={'Confirm'} onClick={this.handleCheckout}/>
+                                    <Modal
+                                        trigger={
+                                            <Button color={'green'}
+                                                    content={'Confirm'}
+                                                    disabled={this.state.disabled}
+                                                    onClick={this.handleCheckout}
+                                            />
+                                        }
+                                    >
+                                        <Header icon={'checkmark'} content={'Confirmed'}/>
+                                        <Modal.Content>
+                                            {
+                                                this.state.transaction ?
+                                                    <Orders res={this.state.res} movies={this.state.carty}/>:
+                                                    <MyLoader/>
+                                            }
+                                        </Modal.Content>
+                                    </Modal>
                                 </Modal.Actions>
                             </Modal>
                         </Container>
