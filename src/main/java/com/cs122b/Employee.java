3df4db64a1f28da957c58f6cd9647e2b66c07c80
Ryan <rjpadil1@uci.edu>
2019-02-15 17:29:19 -0800

    Added employee servlet

---
+++ src/main/java/com/cs122b/Employee.java
@@ -0,0 +1,25 @@
+package com.cs122b;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Employee", urlPatterns = "/employee")
+public class Employee extends HttpServlet {
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Helper.corsFix(resp,req);
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
