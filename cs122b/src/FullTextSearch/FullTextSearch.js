8517ef0ae0f3865f3556e4a4e6e18a5456ec3fb9
Ryan <rjpadil1@uci.edu>
2019-02-25 12:34:03 -0800

    more full search things

--- cs122b/src/FullTextSearch/FullTextSearch.js
+++ cs122b/src/FullTextSearch/FullTextSearch.js
@@ -1,37 +1,44 @@
 import React, { Component } from 'react';
 import {Search} from 'semantic-ui-react';
 import _ from 'lodash';
+import AwesomeDebouncePromise from "awesome-debounce-promise";
 
+const search = value => fetch('https://'+window.location.hostname+':8443/cs122b/fullsearch?query='+value, {
+  method: 'GET',
+  credentials: 'include'
+}).then(
+  (res) => res.json()
+).then(
+  data => {
+    let movies = [];
+    Object.keys(data['movies']).forEach(function(key){
+      movies.push({
+        key: data['movies'][key]['id'],
+        title: data['movies'][key]['title'],
+        description: 'Director: '+data['movies'][key]['director']+', '+data['movies'][key]['year']
+      })
+    });
+    return movies;
+  }
+);
+const searchDebounced = AwesomeDebouncePromise(search, 400);
 class FullTextSearch extends Component{
   componentWillMount() {
     this.resetComponent()
   }
-
-  resetComponent = () => this.setState({ isLoading: false, results: [], value: '' });
+  resetComponent = () => this.setState({ isLoading: false, results: []});
   handleResultSelect = (e, { result }) => this.setState({ value: result.title });
-
   handleSearchChange = (e, { value }) => {
     this.setState({ isLoading: true, value });
-
-    setTimeout(() => {
-      if (this.state.value.length < 1) return this.resetComponent();
-
+    setTimeout(async () => {
+      if (this.state.value.length < 3) return this.resetComponent();
+      const result = await searchDebounced(value);
       this.setState({
         isLoading: false,
-      }, this.search(value))
+        results: result
+      });
     }, 400)
   };
-  search(value){
-    fetch('https://'+window.location.hostname+':8443/cs122b/fullsearch?query='+value, {
-      method: 'GET',
-      credentials: 'include'
-    }).then(
-      (res) => res.json()
-    ).then(
-      data => this.setState({results: data['movies']})
-    );
-    console.log(this.state.results);
-  }
   render(){
     return(
       <Search
