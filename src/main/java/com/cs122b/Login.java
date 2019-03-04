211d87603b3508bfbbaccdebaf25f73e2b2a835b
Ryan <rjpadil1@uci.edu>
2019-03-03 16:36:30 -0800

    GOT SSL CERTS SO SO LONG ANDROID DEV HAHAHAHAH

--- src/main/java/com/cs122b/Login.java
+++ src/main/java/com/cs122b/Login.java
@@ -13,61 +13,62 @@ import java.util.Date;
 
 @WebServlet(name = "Login", urlPatterns = "/login")
 public class Login extends HttpServlet {
-    @Override
-    protected void doOptions(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        Helper.corsFix(resp,req);
-        resp.setStatus(HttpServletResponse.SC_OK);
-    }
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("application/json");
-        Helper.corsFix(response, request);
-        PrintWriter ret = response.getWriter();
-        login(request, ret, response);
-    }
+  @Override
+  protected void doOptions(HttpServletRequest req, HttpServletResponse resp)
+          throws ServletException, IOException {
+    Helper.corsFix(resp, req);
+    resp.setStatus(HttpServletResponse.SC_OK);
+  }
 
-    private void login(HttpServletRequest request, PrintWriter ret, HttpServletResponse response) {
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-            Connection connection = Helper.connection();
-            String username = request.getParameter("username");
-            String password = request.getParameter("password");
-            BufferedReader r = request.getReader();
-            StringBuilder sbuild = new StringBuilder();
-            String param = null;
-            while((param = r.readLine()) != null){
-                sbuild.append(param);
-            }
-            String userAgent = request.getHeader("User-Agent");
-            JSONObject credentials = new JSONObject();
-            if(username == null) {
-                credentials = new JSONObject(sbuild.toString());
-                // Only do recaptcha if not android
-                String gRecaptchaResponse = credentials.getString("g_recaptcha_response");
-                System.out.println("gRecaptchaResponse=" + gRecaptchaResponse);
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("application/json");
+    Helper.corsFix(response, request);
+    PrintWriter ret = response.getWriter();
+    login(request, ret, response);
+  }
 
-                // Verify reCAPTCHA
-                if (EmployeeLogin.RecaptchaVerify(ret, gRecaptchaResponse)) return;
-            }
-            else{
-                credentials.put("username", username);
-                credentials.put("password", password);
-            }
-            credentials = Helper.isValidUser(connection, credentials);
-            EmployeeLogin.VerifyCredentials(request, response, credentials);
-            connection.close();
-            ret.println(credentials);
-            ret.flush();
+  private void login(HttpServletRequest request, PrintWriter ret, HttpServletResponse response) {
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+      Connection connection = Helper.connection();
+      String username = request.getParameter("username");
+      String password = request.getParameter("password");
+      BufferedReader r = request.getReader();
+      StringBuilder sbuild = new StringBuilder();
+      String param = null;
+      while ((param = r.readLine()) != null) {
+        sbuild.append(param);
+      }
+      String userAgent = request.getHeader("User-Agent");
+      JSONObject credentials = new JSONObject();
+      if (username == null) {
+        credentials = new JSONObject(sbuild.toString());
+        // Only do recaptcha if not android
+        if (userAgent != null && userAgent.contains("Android")) {
+          String gRecaptchaResponse = credentials.getString("g_recaptcha_response");
+          System.out.println("gRecaptchaResponse=" + gRecaptchaResponse);
+
+          // Verify reCAPTCHA
+          if (EmployeeLogin.RecaptchaVerify(ret, gRecaptchaResponse)) return;
         }
-        catch (Exception e){
-            JSONObject error = new JSONObject();
-            error.put("error",e);
-            ret.println(error);
-            ret.flush();
-        }
+      } else {
+        credentials.put("username", username);
+        credentials.put("password", password);
+      }
+      credentials = Helper.isValidUser(connection, credentials);
+      EmployeeLogin.VerifyCredentials(request, response, credentials);
+      connection.close();
+      ret.println(credentials);
+      ret.flush();
+    } catch (Exception e) {
+      JSONObject error = new JSONObject();
+      error.put("error", e);
+      ret.println(error);
+      ret.flush();
     }
+  }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EmployeeLogin.CheckLogin(request, response);
-    }
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    EmployeeLogin.CheckLogin(request, response);
+  }
 }
